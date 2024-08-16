d78e39d2d4e9bd0d58d3e328e32bfdfbffd17822
Cory Burmahl <beenters@gmail.com>
2024-08-16 08:49:39 -0700

    Collision

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Importing Pygame
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -31,6 +32,11 @@ def main():
         # Update the player position
         for obj in updatable:
             obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                sys.exit()
 
         screen.fill("black")
         
